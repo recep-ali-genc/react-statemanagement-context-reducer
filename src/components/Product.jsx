ea7f2ea7b22cd4f54eca3ec08bb28e6a763dc79a
Recep Genc <recep@appcircle.io>
2024-12-28 20:59:43 +0300

    reformat code

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,24 +1,27 @@
 export default function Product({
-  id,
-  image,
-  title,
-  price,
-  description,
-  onAddToCart,
+	id,
+	image,
+	title,
+	price,
+	description,
+	onAddToCart,
 }) {
-  return (
-    <article className="product">
-      <img src={image} alt={title} />
-      <div className="product-content">
-        <div>
-          <h3>{title}</h3>
-          <p className='product-price'>${price}</p>
-          <p>{description}</p>
-        </div>
-        <p className='product-actions'>
-          <button onClick={() => onAddToCart(id)}>Add to Cart</button>
-        </p>
-      </div>
-    </article>
-  );
+	return (
+		<article className='product'>
+			<img
+				src={image}
+				alt={title}
+			/>
+			<div className='product-content'>
+				<div>
+					<h3>{title}</h3>
+					<p className='product-price'>${price}</p>
+					<p>{description}</p>
+				</div>
+				<p className='product-actions'>
+					<button onClick={() => onAddToCart(id)}>Add to Cart</button>
+				</p>
+			</div>
+		</article>
+	);
 }
